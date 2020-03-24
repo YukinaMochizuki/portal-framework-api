6afbb0b3650b56441633c04104b07b0c60927acb
MochizukiYukina <1p41p4jejo@gmail.com>
2020-03-24 23:27:46 +0800

    Add scan class function on module and step manager

--- src/main/java/tw/yukina/portalframework/api/module/ModuleManager.java
+++ src/main/java/tw/yukina/portalframework/api/module/ModuleManager.java
@@ -2,5 +2,4 @@ package tw.yukina.portalframework.api.module;
 
 public interface ModuleManager {
 
-
 }
---
+++ src/main/java/tw/yukina/portalframework/api/step/StepManager.java
@@ -0,0 +1,5 @@
+package tw.yukina.portalframework.api.step;
+
+public interface StepManager{
+
+}
